ae007fb28f19d52c6c39a341356a192462af3d61
Leto Atreides <Vladimir Harkonnen@VladimirHarkonnen>
2018-10-09 21:25:19 +0200

    Rango de 10 - 50

---
+++ src/Aciertos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Aciertos {
+
+    public static void main(String[] args) {
+        int n = 0,cont = 0;
+        int x = new Random().nextInt((50 - 10) + 1) + 10;/*(int) (50 *Math.random());*/
+        System.out.print(x);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();
+    }
+}
